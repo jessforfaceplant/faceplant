e7e1e5cbd5f55b46a7f83b72c02df22feadff5ba
Erik Hauner <ehauner@gmail.com>
2016-03-27 16:36:45 -0700

    Started template for plant profile

---
+++ profile.php
@@ -0,0 +1,78 @@
+<html>
+
+	<head>
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
+		<link href='https://fonts.googleapis.com/css?family=Arvo' rel='stylesheet' type='text/css'>
+		<link rel="stylesheet" type="text/css" href="style.css" />
+		<script src="scripts.js"></script>
+	</head>
+
+	<body>
+		<p class="wrapper" id="logo" onmouseover="this.innerHTML = 'FACEPLANT ;D'" onmouseout="this.innerHTML = 'FACEPLANT :D'">FACEPLANT :D</p>
+		<hr><hr>
+		<p id="com_name">Sunflower</p>
+		<p id="sci_name">Helianthus annus</p>
+		<p id="cultivar">Italian White</p>
+		<p id="description">A tall North American plant of the daisy family, with very large golden-rayed flowers.</p>
+		<table style="width:100%">
+			<tr>
+				<td>Edible</td>
+				<td id="edible">Yes</td> 
+			</tr>
+			<tr>
+				<td>Medicinal</td>
+				<td id="medicinal">Yes</td> 
+			</tr>
+			<tr>
+				<td>Petsafe</td>
+				<td id="petsafe">Yes</td> 
+			</tr>
+			<tr>
+				<td>Height</td>
+				<td id="height">High</td> 
+			</tr>
+			<tr>
+				<td>Width</td>
+				<td id="width">High</td> 
+			</tr>
+			<tr>
+				<td>Light</td>
+				<td id="light">High</td> 
+			</tr>
+			<tr>
+				<td>Growth period</td>
+				<td id="growthperiod">April to September</td> 
+			</tr>
+			<tr>
+				<td>Temperature</td>
+				<td id="temperature">4C to 40C</td> 
+			</tr>
+			<tr>
+				<td>Moisture</td>
+				<td id="moisture">Medium</td> 
+			</tr>
+			<tr>
+				<td>N:P:K</td>
+				<td id="npk">L:M:M</td> 
+			</tr>
+			<tr>
+				<td>Humus</td>
+				<td id="humus">Medium</td> 
+			</tr>
+			<tr>
+				<td>Clay</td>
+				<td id="clay">Medium</td> 
+			</tr>
+			<tr>
+				<td>pH</td>
+				<td id="ph">Medium</td> 
+			</tr>
+		</table>
+		
+		<div class="searchColumn">
+			
+		</div>
+		<div>
+		</div>
+	</body>
+</html>

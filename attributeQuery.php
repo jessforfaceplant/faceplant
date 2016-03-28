39f2d55cceaa4650f6716c7710e3819f280ad09a
Erik Hauner <ehauner@gmail.com>
2016-03-28 13:04:58 -0700

    Added plant_id to profile link and displayed sci_name in results

--- attributeQuery.php
+++ attributeQuery.php
@@ -21,7 +21,7 @@
 	// Create connection to Oracle
 	$conn = oci_connect("ora_o1c0b", "a55307145", "ug");
 
-	$query = 'select distinct com_name, cultivar from climates cl, soils s, has_colour co, plants p where p.plant_id = co.plant_id and p.climate_id = cl.climate_id and p.soil_id = s.soil_id' . $attributeQuery;
+	$query = 'select distinct p.plant_id, com_name, sci_name, cultivar from climates cl, soils s, has_colour co, plants p where p.plant_id = co.plant_id and p.climate_id = cl.climate_id and p.soil_id = s.soil_id' . $attributeQuery . ' order by com_name asc';
 	//echo($query);
 	$stid = oci_parse($conn, $query);
 	$r = oci_execute($stid);
@@ -32,22 +32,23 @@
 	print '<table border="1">';
 	while ($row = oci_fetch_array($stid, OCI_RETURN_NULLS+OCI_ASSOC)) {
 		$numresults++;
+		$plant_id = $row['PLANT_ID'];
 		print '<tr>';
 		foreach ($row as $item) {
-			print '<td>'. '<a href="profile.php?name=' . ($item !== null ? htmlentities($item, ENT_QUOTES) : '&nbsp') . '">' . ($item !== null ? htmlentities($item, ENT_QUOTES) : '&nbsp'). '</a>' . '</td>';
+			print '<td>'. '<a href="profile.php?id=' . $plant_id . '">' . ($item !== null ? htmlentities($item, ENT_QUOTES) : '&nbsp'). '</a>' . '</td>';
 		}
 		print '</tr>';
 	}
 	print '</table>';
 	
 	if ($numresults == 0) {
-		print '<p style="text-align:center;">No plants match your search :(</p>';
+		print '<p style="text-align:center;">The database seems to be empty :(</p>';
 	}
 	elseif ($numresults == 1) {
-		print '<p style="text-align:center;">' . $numresults . ' match</p>';
+		print '<p style="text-align:center;">' . $numresults . ' plant</p>';
 	}
 	else {
-		print '<p style="text-align:center;">' . $numresults . ' matches</p>';
+		print '<p style="text-align:center;">' . $numresults . ' plants</p>';
 	}
 ?>
 </html>

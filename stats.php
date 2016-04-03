27f4608bc8b792a0ef816de51ae665ef97a87c01
Erik Hauner <ehauner@gmail.com>
2016-04-03 14:00:20 -0700

    Sara did the most liked and least liked plants in stats page

--- stats.php
+++ stats.php
@@ -12,8 +12,9 @@
 		$queryMed = "select count(*) from plants where medicinal = 'Y' group by medicinal";
 		$queryEd = "select count(*) from plants where edible = 'Y' group by edible";
 		$queryPet = "select count(*) from plants where petsafe = 'Y' group by petsafe";
-		$queryPop = "with temp as (select plant_id, count(*) as total from favourites group by plant_id) select t.total, p.sci_name, p.com_name, p.cultivar from temp t, plants pwhere t.total = (select max(t.total) from temp t) and t.plant_id = p.plant_id";
-		$queryNop = "with temp as (select plant_id, count(*) as total from favourites group by plant_id) select t.total, p.sci_name, p.com_name, p.cultivar from temp t, plants pwhere t.total = (select min(t.total) from temp t) and t.plant_id = p.plant_id";
+		$queryPop = "with temp as (select plant_id, count(*) as total from favourites group by plant_id) select p.plant_id, t.total, p.sci_name, p.com_name, p.cultivar from temp t, plants p where t.total = (select max(t.total) from temp t) and t.plant_id = p.plant_id";
+		$queryNop = "with temp as (select plant_id, count(*) as total from favourites group by plant_id) select p.plant_id, t.total, p.sci_name, p.com_name, p.cultivar from temp t, plants p where t.total = (select min(t.total) from temp t) and t.plant_id = p.plant_id";
+
 		// Create connection to Oracle
 		$conn = oci_connect("ora_o1c0b", "a55307145", "ug");
 
@@ -35,18 +36,6 @@
 			$pet_count = $row['COUNT(*)'];
 		}
 		
-		$stid = oci_parse($conn, $queryPop);
-		$r = oci_execute($stid);
-		while ($row = oci_fetch_array($stid, OCI_RETURN_NULLS+OCI_ASSOC)) {
-			$pop_count = $row['COUNT(*)'];
-			echo $row['COUNT(*)'];
-		}
-		
-		$stid = oci_parse($conn, $queryNop);
-		$r = oci_execute($stid);
-		while ($row = oci_fetch_array($stid, OCI_RETURN_NULLS+OCI_ASSOC)) {
-			$nop_count = $row['COUNT(*)'];
-		}
 	?>
 	
 	
@@ -67,15 +56,88 @@
 					print '<td>' . $pet_count . '</td></tr>';
 				?>
 			</table>
-			<p class="subhead">Most popular</p>
-			<?php
-				print '<a>' . $pop_count . '</a>';
-			?>
-			<p class="subhead">Least popular</p>
-			<?php
-				print '<a>' . $nop_count . '</a>';
-			?>
-
+			<div style="padding-top: 30px;">
+				<p class="subhead">Sup Faceplant?! What plants are the most and least popular?</p>	
+				<p class="subhead">Most Popular:</p>
+				<?php
+					$stid = oci_parse($conn, $queryPop);
+					$r = oci_execute($stid);
+					$numresults = 0;
+					// Fetch each row in an associative array
+					print '<form action="stats.php" method="get">';
+					print '<table>';
+					print '<td style="background-color: #d9d9d9;">Total</td>';
+					print '<td style="background-color: #d9d9d9;">Scientific Name</td>';
+					print '<td style="background-color: #d9d9d9;">Common Name</td>';
+					print '<td style="background-color: #d9d9d9;">Cultivar</td>';
+					while ($row = oci_fetch_array($stid, OCI_RETURN_NULLS+OCI_ASSOC)) {
+						$numresults++;
+						$plant_id = $row['PLANT_ID'];
+						print '<tr>';
+						foreach ($row as $item) {
+							if ($item != $plant_id) 
+							{
+								print '<td>'. '<a href="profile.php?id=' . $plant_id . '">' . ($item !== null ? htmlentities(ucfirst($item), ENT_QUOTES) : '&nbsp'). '</a>' . '</td>';
+							}
+							
+						}
+						print '</tr>';
+					}
+					print '</table>';
+				    //print '<input type="submit" style="margin-top: 10px;" value="Add to favourites">';
+					print '</form>';
+	
+					if ($numresults == 0) {
+						print '<p style="text-align:center; margin-top: 0px;">No plants match your query :(</p>';
+					}
+					elseif ($numresults == 1) {
+						print '<p style="text-align:center; margin-top: 0px;">' . $numresults . ' plant</p>';
+					}
+					else {
+						print '<p style="text-align:center; margin-top: 0px;">' . $numresults . ' plants</p>';
+					}
+				?>
+			</div>
+			<div>
+				<p class="subhead">Least Popular:</p>
+				<?php
+					$stid = oci_parse($conn, $queryNop);
+					$r = oci_execute($stid);
+					$numresults = 0;
+					// Fetch each row in an associative array
+					print '<form action="stats.php" method="get">';
+					print '<table>';
+					print '<td style="background-color: #d9d9d9;">Total</td>';
+					print '<td style="background-color: #d9d9d9;">Scientific Name</td>';
+					print '<td style="background-color: #d9d9d9;">Common Name</td>';
+					print '<td style="background-color: #d9d9d9;">Cultivar</td>';
+					while ($row = oci_fetch_array($stid, OCI_RETURN_NULLS+OCI_ASSOC)) {
+						$numresults++;
+						$plant_id = $row['PLANT_ID'];
+						print '<tr>';
+						foreach ($row as $item) {
+							if ($item != $plant_id) 
+							{
+								print '<td>'. '<a href="profile.php?id=' . $plant_id . '">' . ($item !== null ? htmlentities(ucfirst($item), ENT_QUOTES) : '&nbsp'). '</a>' . '</td>';
+							}
+						}
+						print '</tr>';
+					}
+					print '</table>';
+				    //print '<input type="submit" style="margin-top: 10px;" value="Add to favourites">';
+					print '</form>';
+	
+					if ($numresults == 0) {
+						print '<p style="text-align:center; margin-top: 0px;">No plants match your query :(</p>';
+					}
+					elseif ($numresults == 1) {
+						print '<p style="text-align:center; margin-top: 0px;">' . $numresults . ' plant</p>';
+					}
+					else {
+						print '<p style="text-align:center; margin-top: 0px;">' . $numresults . ' plants</p>';
+					}
+				?>
+			</div>
 		</div>
 		
 		
